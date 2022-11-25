bbf5a0481e6015fe436d5c84abdc59aba7988f60
CoupeDesCampagnes <94222253+CoupeDesCampagnes@users.noreply.github.com>
2022-11-25 22:02:09 +0100

    Update url.js

--- dossier/scripts/url.js
+++ dossier/scripts/url.js
@@ -32,39 +32,11 @@ function color_mixer(r,g,b){
 
 function getUrl(chaine){
     const hash=nombre(chaine)
-    if (hash===474269515){
-        return "https://docs.google.com/spreadsheets/d/1VkdV95-"+chaine+"-gPU7VOcmJGITTzZ0tGexYBY/edit?usp=sharing"
-    }
-    else if (hash===298891818){
+    if (hash===39962494){
         return "http://coupedescampagnes.github.io/CrackingTheBox/dossier/salle/couloir.html"
     }
-    else if(hash===980574260){
-        return "http://coupedescampagnes.github.io/CrackingTheBox/dossier/salle/"+chaine+".html"
-    }
-    else if(hash===288073393){
-        color_mixer(225,80,42)
-        return "Easter Egg"
-    }else if(hash===517202362){
-        color_mixer(168,233,255)
-        return "Easter Egg"
-    }else if(hash===361866239){
-        color_mixer(134,1,17)
-        return "Easter Egg"
-    }else if(hash===41587392){
-        color_mixer(255,215,0)
-        return "Easter Egg"
-    }else if(hash===236477960){
-        color_mixer(219,0,132)
-        return "Easter Egg"
-    }else if(hash===689272931){
-        color_mixer(239,239,239)
-        return "Easter Egg"
-    }else if(hash===122118945){
-        color_mixer(255,0,0)
-        return "Easter Egg"
-    }else if(hash===1548920){
-        color_mixer(240,219,255)
-        return "Easter Egg"
+    else if (hash===44285436){
+        return "http://coupedescampagnes.github.io/CrackingTheBox/dossier/enigma/notch.html"
     }
     else {
         return hash
