5137652ea1170a31e51fe3fca47836fd26147d81
CoupeDesCampagnes <94222253+CoupeDesCampagnes@users.noreply.github.com>
2025-12-02 16:06:22 +0100

    Update url.js

--- dossier/scripts/url.js
+++ dossier/scripts/url.js
@@ -57,7 +57,7 @@ function getUrl(chaine){
 
 document.querySelector("#button").addEventListener("click", (event) => {
     chaine = document.querySelector("#code").value;
-    texte = fetch("https://www.cdc.ginfo.centrale-med.fr/api/"+chaine);
+    texte = await fetch("https://www.cdc.ginfo.centrale-med.fr/api/"+chaine);
     document.querySelector("#value").textContent = texte;
     event.preventDefault();
 })
